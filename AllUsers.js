8128f46c9ceafba8e4f0bc2233eb855b53364233
Gauravvgithub <gauravraj7011@gmail.com>
2025-05-22 13:00:53 +0530

    addUsers in DB

---
+++ AllUsers.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    "id": 1,
+    "name": "Gaurav",
+    "age": "22",
+    "gender": "male",
+    "device": "windows",
+    "date": "21-03-2025"
+  },
+  {
+    "id": 2,
+    "name": "Hansu",
+    "age": "18",
+    "gender": "male",
+    "device": "windows",
+    "date": "21-03-2025"
+  },
+  {
+    "id": 3,
+    "name": "Rishi",
+    "age": "25",
+    "gender": "male",
+    "device": "ios",
+    "date": "21-03-2025"
+  },
+  {
+    "id": 4,
+    "name": "Raj",
+    "age": "30",
+    "gender": "male",
+    "device": "linux",
+    "date": "21-03-2025"
+  },
+  {
+    "id": 5,
+    "name": "Ronak",
+    "age": "32",
+    "gender": "male",
+    "device": "windows",
+    "date": "21-03-2025"
+  },
+  {
+    "id": 6,
+    "name": "Ajay",
+    "age": "34",
+    "gender": "male",
+    "device": "windows",
+    "date": "21-03-2025"
+  },
+  {
+    "id": 7,
+    "name": "jyot",
+    "age": "28",
+    "gender": "male",
+    "device": "ios",
+    "date": "21-03-2025"
+  },
+  {
+    "id": 8,
+    "name": "Mehrav",
+    "age": "22",
+    "gender": "male",
+    "device": "windows",
+    "date": "21-03-2025"
+  },
+  {
+    "id": 9,
+    "name": "Godwin",
+    "age": "28",
+    "gender": "male",
+    "device": "windows",
+    "date": "21-03-2025"
+  },
+  {
+    "id": 10,
+    "name": "Umesh",
+    "age": "28",
+    "gender": "male",
+    "device": "ios",
+    "date": "21-03-2025"
+  }
+]
--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const app = express();
+const path = require("path")
 const DataBase = require("./models/connection");
+const AllUsers = require("./AllUsers")
+
 
 const PORT = 3000;
 
 app.use(express.json()); // accepts json data
 
+app.use(express.static("public"));
+
 app.get("/",(request, response)=>{
     response.send("this is home page")
+})
+
+app.get("/showAllUsers",(request, response)=>{
+    response.json(AllUsers)
 })
 
 app.post("/addUser", (request, response) => {
